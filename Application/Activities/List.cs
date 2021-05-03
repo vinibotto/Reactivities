bc9d33655da46b2402c94152af1e6f16d1236759
Vinicius Botto <vinibotto@gmail.com>
2021-05-03 21:40:17 +0100

    End of Section 5

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -26,19 +26,6 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (var i = 0; i < 10; i++) 
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }catch (Exception ex) when (ex is TaskCanceledException)
-                {
-                    _logger.LogInformation("Task was cancelled");
-                }
-
                 return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
