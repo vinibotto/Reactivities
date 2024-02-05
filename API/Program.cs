d68b09b1f32edb3b011d23db5cce7ac1e3167c09
vinicius <vinibotto@gmail.com>
2024-02-05 23:46:59 +0100

    Security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -28,10 +28,32 @@ await app.SeedDatabase();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseXContentTypeOptions();
+app.UseReferrerPolicy(opt => opt.NoReferrer());
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny());
+app.UseCsp(opt => opt
+    .BlockAllMixedContent()
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+    .FontSources(s=> s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+    .FormActions(s=>s.Self())
+    .FrameAncestors(s=>s.Self())
+    .ImageSources(s=>s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+    .ScriptSources(s=>s.Self())
+);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+}
+else 
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseHttpsRedirection();
