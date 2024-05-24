898a19be0d7f12b9aa06ac0d7fe5019aa7e7daf4
vinicius <vinibotto@gmail.com>
2024-05-24 16:03:09 +0200

    removing password restriction

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,7 +9,6 @@ namespace API.DTOs
         public string Email { get; set; }
 
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string Password { get; set; }
 
         [Required]
